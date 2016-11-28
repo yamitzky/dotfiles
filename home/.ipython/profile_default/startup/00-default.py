caa408bb322f5156aaedd7519b6ca29c2b2d37d8
Yamitzky <negiga@gmail.com>
2016-11-28 10:20:37 +0900

    [ipython]fix to suppress alert

--- home/.ipython/profile_default/startup/00-default.py
+++ home/.ipython/profile_default/startup/00-default.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import division, print_function
 
-from six.moves import filter, map, range, zip
-from matplotlib.pyplot import ion as __ion
+try:
+    from six.moves import filter, map, range, zip
+    from matplotlib.pyplot import ion as __ion
 
-__ion()
+    __ion()
+except Exception:
+    pass
