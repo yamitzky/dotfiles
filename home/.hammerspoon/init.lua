b5e33ad32b278f164d4e3a3d29c9737d9ff14186
Yamitzky <negiga@gmail.com>
2016-11-29 16:08:51 +0900

    [hammerspoon]

--- home/.hammerspoon/init.lua
+++ home/.hammerspoon/init.lua
@@ -1,4 +1,36 @@
-hs.hotkey.bind({"ctrl"}, 102, function()
+function pressKey(modifier, key)
+  hs.eventtap.event.newKeyEvent(modifier, key, true):post()
+  hs.timer.usleep(100)
+  hs.eventtap.event.newKeyEvent(modifier, key, false):post()
+end
+function launchOrHide(launchAppName, activeAppName)
+  app = hs.application.frontmostApplication()
+  if app:name() == activeAppName then
+    app:hide()
+  else
+    hs.application.launchOrFocus(launchAppName)
+  end
+end
+
+hs.hotkey.bind({"ctrl"}, 102, function() end)
+hs.hotkey.bind({"shift"}, 102, function() end)
+
+hs.hotkey.bind({"cmd", "alt", "shift"}, "J", function()
+  pressKey({"cmd", "shift"}, "[")
 end)
-hs.hotkey.bind({"shift"}, 102, function()
+hs.hotkey.bind({"cmd", "alt", "shift"}, "L", function()
+  pressKey({"cmd", "shift"}, "]")
+end)
+
+hs.hotkey.bind({"cmd"}, "1", function()
+  launchOrHide("iTerm 2", "iTerm2")
+end)
+hs.hotkey.bind({"cmd"}, "2", function()
+  launchOrHide("Atom", "Atom")
+end)
+hs.hotkey.bind({"cmd"}, "3", function()
+  launchOrHide("Google Chrome", "Google Chrome")
+end)
+hs.hotkey.bind({"cmd"}, "4", function()
+  launchOrHide("IntelliJ IDEA CE", "IntelliJ IDEA")
 end)
